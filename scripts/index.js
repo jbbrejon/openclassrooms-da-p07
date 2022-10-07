5bf1eb9ebd19cbd5dd98e81bbb9354deef4998e3
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-10-07 11:45:46 +0200

    add DOM elements to monitor and new instance of Input class

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+// DOM elements
+const searchForm = document.getElementById("global-search");
+let searchInput = document.getElementById("item");
+
+// Create new instance of Search class
+let myInput = new Input();
