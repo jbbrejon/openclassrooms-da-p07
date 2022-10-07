87f45d37c701aaff37795c5de915c5fb25f8838f
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-10-07 14:55:37 +0200

    add .isId(), .getRecipesbyName() and .renderResults() methods

--- scripts/recipes.js
+++ scripts/recipes.js
@@ -3,8 +3,10 @@ class Recipes {
     constructor(data) {
         this._data = data;
         this._query = "";
+        this._results = [];
 
     }
+
     // Check if query is at least 3 characters long
     isThreeChar() {
         if (this._query.length > 2) {
@@ -19,11 +21,54 @@ class Recipes {
     globalSearch(input) {
         this._query = input;
         if (this.isThreeChar()) {
-            console.log(this._query);
+            // Reset results array
+            this._results = [];
+            // Remove existing cards from DOM
+            this.removeCards();
+            // Search by name (title)
+            this.getRecipesbyName();
+            // Add results to DOM
+            this.renderResults();
+        }
+        else {
+            // Display all recipes
+            this.renderAll();
         }
     }
+    // Check if recipe already exists in filtered results
+    isId(id) {
+        let isFound = this._results.some(recipe => recipe.id == id);
+        return isFound;
+    }
+
+    // Search by name
+    getRecipesbyName() {
+        this._data.forEach(recipe => {
+            let recipeId = recipe.id;
+            let recipeName = recipe.name;
+
+            if (recipeName.toLowerCase().includes(this._query)) {
+                if (!this.isId(recipeId)) {
+                    this._results.push(recipe)
+                    return recipe;
+                }
+            }
+            else {
+                return "No recipe found";
+            }
+        });
+    }
+
     renderAll() {
         this._data.forEach(recipe => {
+            let article = new RecipeTemplate(recipe);
+            article.createCard();
+            return article
+        });
+    }
+
+    renderResults() {
+        this._results.forEach(recipe => {
             let article = new RecipeTemplate(recipe);
             article.createCard();
             return article
@@ -38,4 +83,5 @@ class Recipes {
         });
         return "Recipe cards removed";
     }
+
 }
