8059966bcf42e792a6b65e98a8fa695b0cdd7d9b
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-10-07 13:27:41 +0200

    update .query() method to call globalSearch method of Recipes class

--- scripts/input.js
+++ scripts/input.js
@@ -1,12 +1,14 @@
 // Class - Search
 class Input {
+
     // Update this._input value (remove extra space and convert to lower case)
     update() {
         this._input = searchInput.value.trim().toLowerCase();
         this.query();
     }
+
     // Initiate query
     query() {
-        console.log(this._input);
+        myRecipes.globalSearch(this._input);
     }
 }
