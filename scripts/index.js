d85430445e5d4a9b0c7767883d188b618362837e
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-10-07 13:26:19 +0200

    create new instance of Recipes class

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,5 @@ let searchInput = document.getElementById("item");
 
 // Create new instance of Search class
 let myInput = new Input();
+// Create new instance of Recipes class
+let myRecipes = new Recipes(recipes);
