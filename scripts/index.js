5b38ea7daeb9a6815fc11576730d75b39ce27545
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2022-10-09 22:18:36 +0200

    merge from main branch

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 // DOM elements
 const searchForm = document.getElementById("global-search");
 let searchInput = document.getElementById("item");
+let searchIngredients = document.getElementById("item-ingredients");
+let searchAppliances = document.getElementById("item-appliances");
+let searchUstensils = document.getElementById("item-ustensils");
 
 // Create new instance of Search class
 let myInput = new Input();
